4eadb6e6db552ea1d41c156543c3bee548412683
William <william.k.withers@gmail.com>
2022-10-14 14:12:44 -0500

    added actions under bar

--- src/main/resources/static/js/battleLogic.js
+++ src/main/resources/static/js/battleLogic.js
@@ -23,9 +23,15 @@ function battleLoop(hero, enemy) {
             ${CharacterInfoBar(enemy.currentHp, enemy.maxHp, enemy.name)}
             <img class="battle-image" src="https://previews.123rf.com/images/imazydreams/imazydreams1409/imazydreams140900199/31952459-goblin.jpg" alt="enemy">
         </div>
-        <div id="hero-section" class="d-flex jusitfy-content-between align-items-start my-4">
-            <img class="battle-image" src="https://www.vhv.rs/dpng/d/527-5273513_warrior-character-image-black-desert-character-png-transparent.png" alt="hero">
-            ${CharacterInfoBar(hero.currentHp, hero.maxHp, "withers56")}
+        <div id="hero-section" class="row my-4">
+            <img class="battle-image col-6" src="https://www.vhv.rs/dpng/d/527-5273513_warrior-character-image-black-desert-character-png-transparent.png" alt="hero">
+            <div class="col-6 d-flex flex-column align-items-end">
+                ${CharacterInfoBar(hero.currentHp, hero.maxHp, "withers56")}
+                <div class="mx-5">
+                    <button class="btn btn-primary me-3">attack</button>
+                    <button class="btn btn-primary ms-3">attack</button>
+                </div>
+            </div>
         </div>
         <div id="action-section">
             
