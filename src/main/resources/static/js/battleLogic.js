59211042f8d8b48441404bfe1d7a633878e0b1c5
William <william.k.withers@gmail.com>
2022-10-14 17:12:58 -0500

    added chatbox and actions

--- src/main/resources/static/js/battleLogic.js
+++ src/main/resources/static/js/battleLogic.js
@@ -27,8 +27,18 @@ function battleLoop(hero, enemy) {
             <img class="battle-image" src="https://www.vhv.rs/dpng/d/527-5273513_warrior-character-image-black-desert-character-png-transparent.png" alt="hero">
             ${CharacterInfoBar(hero.currentHp, hero.maxHp, "withers56")}
         </div>
-        <div id="action-section">
-            
+        <div id="action-section" class="d-flex align-items-center">
+            <div class="game-chat p-2">text</div>
+            <div class="actions p-2">
+                <div class="top-actions d-flex justify-content-around my-2">
+                    <button class="btn btn-primary" id="attack">Attack</button>
+                    <button class="btn btn-primary" id="bag">Bag</button>
+                </div>
+                <div class="bottom-actions d-flex justify-content-around my-2">
+                    <button class="btn btn-primary" id="spacer">Spacer</button>
+                    <button class="btn btn-primary" id="flea">Flea!</button>
+                </div>
+            </div>
         </div>
     </div>
     `
@@ -36,4 +46,35 @@ function battleLoop(hero, enemy) {
     //sets hero and enemies healthbars to current helath values
     setHealthBar(enemy.currentHp, enemy.maxHp, enemy.name)
     setHealthBar(hero.currentHp, hero.maxHp, "withers56")
+    //add actionjs listeners
+    addActionBtnListeners();
+
+}
+
+function addActionBtnListeners() {
+    attackBtnListener()
+    bagBtnListener()
+    spacerBtnListener()
+    fleaBtnListener()
+}
+
+function attackBtnListener() {
+    document.querySelector("#attack").addEventListener("click", function (){
+        console.log('clicked attack!')
+    })
+}
+function bagBtnListener() {
+    document.querySelector("#bag").addEventListener("click", function (){
+        console.log('clicked bag!')
+    })
+}
+function spacerBtnListener() {
+    document.querySelector("#spacer").addEventListener("click", function (){
+        console.log('clicked spacer!')
+    })
+}
+function fleaBtnListener() {
+    document.querySelector("#flea").addEventListener("click", function (){
+        console.log('clicked flea!')
+    })
 }
